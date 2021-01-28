38d2adffb18c44399b098394fcb49e1806463dd2
Bekzat <b_molutov@kbtu.kz>
2021-01-28 13:57:21 +0600

    new

---
+++ booleans.py
@@ -0,0 +1,21 @@
+# by Bekzat
+print(1 > 2)
+print(2 > 1)
+print(1 == 2)
+# evaluating
+# true
+print(bool("hey!"))
+print(bool(10))
+print(bool(["apple", "cherry", "banana"]))
+# false
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
+# Check if an object is an integer or not:
+x = 100
+print(isinstance(x, int))
+
