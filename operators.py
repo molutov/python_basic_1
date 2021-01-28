b15b0ab5b700afd2150bde9e79c1c3f0ddf1e92b
Bekzat <b_molutov@kbtu.kz>
2021-01-28 14:42:50 +0600

    new

---
+++ operators.py
@@ -0,0 +1,43 @@
+# by Bekzat
+x = 5
+y = 3
+print(x + y)
+x = 5
+y = 3
+print(x - y)
+x = 5
+y = 3
+print(x * y)
+x = 12
+y = 3
+print(x / y)
+x = 5
+y = 2
+print(x % y)
+x = 2
+y = 5
+print(x ** y) # same as 2*2*2*2*2
+x = 15
+y = 2
+print(x // y) # the floor division
+x = 5
+x &= 0
+print(x) # bitwise and
+x = 5
+x |= 3
+print(x) # bitwise or
+x = 5
+x ^= 3
+print(x) # bitwise xor
+x = 5
+x >>= 3
+print(x) # bitwise right shift
+x = 5
+x <<= 3
+print(x) # bitwise left shift
+# ~ NOT inverts all the bits
+
+
+
+
+
